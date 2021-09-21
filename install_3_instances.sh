2dd9a48a9cb4238188905986d1a381fa33faf675
Alexandru Solopa <a.solopa@unifun.com>
2021-09-21 14:04:32 +0300

    added install_3_instances.sh

---
+++ install_3_instances.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+sudo apt update
+sudo apt install wget -y
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
+sudo apt upgrade -y
+sudo apt install wget git build-essential jq expect -y
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/docker_installer.sh)
+mkdir $HOME/.streamrDocker
+mkdir $HOME/.streamrDocker_2
+mkdir $HOME/.streamrDocker_3
+
+
+cfg_node() {
+  	set timeout 300
+  	spawn docker run -it -v $(cd ~/.$1; pwd):/root/.streamr streamr/broker-node:testnet bin/config-wizard
+  	expect "Do you want to generate"
+  	send -- "\n"
+  	expect "We strongly recommend"
+  	send -- "y\n"
+  	expect "Select the plugins"
+  	send -- "a\n"
+  	expect "Provide a port for the websocket"
+  	send -- "$2\n"
+  	expect "Provide a port for the mqtt"
+  	send -- "$3\n"
+  	expect "Provide a port for the publishHttp"
+  	send -- "$4\n"
+  	expect "Select a path to store"
+  	send -- "\n"
+  	expect eof
+
+  	docker run -it --restart=always --name=streamr_node -d -p $2:$2 -p $4:$4 -p $3:$3 -v $(cd ~/.$1; pwd):/root/.streamr streamr/broker-node:testnet
+
+}
+
+cfg_node streamrDocker 12345 12348 12351
+cfg_node streamrDocker_2 12346 12349 12352
+cfg_node streamrDocker_3 12347 12350 12353
+
+
+
+
+
